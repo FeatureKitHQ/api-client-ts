a267bacfb557d7ac40cf164a9a9cb2c928fa5208
Ibrahim Butt <ibrahimbutt1993@gmail.com>
2024-11-20 21:15:21 +0000

    chore: create initial client

--- git_push.sh
+++ git_push.sh
@@ -37,21 +37,27 @@ git add .
 # Commits the tracked changes and prepares them to be pushed to a remote repository.
 git commit -m "$release_note"
 
-# Sets the new remote
-git_remote=$(git remote)
-if [ "$git_remote" = "" ]; then # git remote not defined
+git branch -M main
+git remote add origin https://github.com/FeatureKitHQ/api-client-ts.gitgit push -u origin main
 
-    if [ "$GIT_TOKEN" = "" ]; then
-        echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
-        git remote add origin https://${git_host}/${git_user_id}/${git_repo_id}.git
-    else
-        git remote add origin https://${git_user_id}:"${GIT_TOKEN}"@${git_host}/${git_user_id}/${git_repo_id}.git
-    fi
+git push -u origin main
 
-fi
 
-git pull origin master
-
-# Pushes (Forces) the changes in the local repository up to the remote repository
-echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
-git push origin master 2>&1 | grep -v 'To https'
+## Sets the new remote
+#git_remote=$(git remote)
+#if [ "$git_remote" = "" ]; then # git remote not defined
+#
+#    if [ "$GIT_TOKEN" = "" ]; then
+#        echo "[INFO] \$GIT_TOKEN (environment variable) is not set. Using the git credential in your environment."
+#        git remote add origin https://${git_host}/${git_user_id}/${git_repo_id}.git
+#    else
+#        git remote add origin https://${git_user_id}:"${GIT_TOKEN}"@${git_host}/${git_user_id}/${git_repo_id}.git
+#    fi
+#
+#fi
+#
+#git pull origin master
+#
+## Pushes (Forces) the changes in the local repository up to the remote repository
+#echo "Git pushing to https://${git_host}/${git_user_id}/${git_repo_id}.git"
+#git push origin master 2>&1 | grep -v 'To https'
